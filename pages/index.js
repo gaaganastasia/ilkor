2b6d286af0d3d2fb78e5d72d4406e51b0ede06be
gaaganastasia <n.young.blood@mail.ru>
2022-05-09 22:17:01 +0700

    head

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './components/App/App';
+import Head from 'next/head';
 
 export default class extends Component {
   render () {
     return (
       <React.StrictMode>
+        <Head>
+          <link rel="shortcut icon" href="../static/favicon.ico" />
+        </Head>
         <App />
       </React.StrictMode>
     )
