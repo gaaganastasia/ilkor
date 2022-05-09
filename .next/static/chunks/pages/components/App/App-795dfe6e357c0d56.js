33d5477b80de0572cad32872e4f9f54e1c7673c7
gaaganastasia <n.young.blood@mail.ru>
2022-05-09 22:26:53 +0700

    link

--- .next/static/chunks/pages/components/App/App-795dfe6e357c0d56.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[606],{8381:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/App/App",function(){return u(6549)}])}},function(n){n.O(0,[261,675,105,927,549,774,888,179],(function(){return _=8381,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
--- pages/components/Footer/Footer.js
+++ pages/components/Footer/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
       <Image className="footer__logo" alt="Логотип" src={logo}></Image>
       <div className="footer__links">
         <a className="footer__link" href="https://t.me/kir354">Telegram</a>
-        <a className="footer__link" href="vk.com/id647668431">VKontakte</a>
+        <a className="footer__link" href="https://vk.com/id647668431">VKontakte</a>
         <a className="footer__link" href="#">Instagram</a>
       </div>
       <p className="footer__up" onClick={scrollToTop}>Наверх</p>
